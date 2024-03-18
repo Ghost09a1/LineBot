f0b6331fbbe28e627c136280588107854dbf1ea2
Ghost09a1 <139347108+Ghost09a1@users.noreply.github.com>
2024-03-18 14:20:52 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,69 @@
+import os
+import sys
+
+from fastapi import Request, FastAPI, HTTPException
+
+from linebot.v3.webhook import WebhookParser
+from linebot.v3.messaging import (
+    AsyncApiClient,
+    AsyncMessagingApi,
+    Configuration,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+
+
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv('2bfb32b405764eb9c73ee8e3ccae43b2', None)
+channel_access_token = os.getenv('trcrGS6lRXZ4+giOYBkTJaGkycZgibtAeLR14XzP3eT1nJmdxhBKudD7E5exTnMEp7CMLPmh2cYc3MWlJq/DS3JW9hcP8WbofO+9M6CHuN5iZ/A68G1PEsgEOA6kdyDfnRhWOZLID8sC38GB996QPgdB04t89/1O/w1cDnyilFU=', None)
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+configuration = Configuration(
+    access_token=channel_access_token
+)
+
+app = FastAPI()
+async_api_client = AsyncApiClient(configuration)
+line_bot_api = AsyncMessagingApi(async_api_client)
+parser = WebhookParser(channel_secret)
+
+
+@app.post("/callback")
+async def handle_callback(request: Request):
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = await request.body()
+    body = body.decode()
+
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        raise HTTPException(status_code=400, detail="Invalid signature")
+
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessageContent):
+            continue
+
+        await line_bot_api.reply_message(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=f"you just typed {event.message.text}")]
+            )
+        )
+
+    return 'OK'
